1f2f0b6d3a567f04739fe99d3da85eeb6b066c69
Seong min <pose1358@naver.com>
2021-06-13 15:07:06 +0900

    행렬의 덧셈

---
+++ Test1.java
@@ -0,0 +1,14 @@
+public class Test1 {
+	static int iot;
+    public static void main(String[] args){
+        int uc=11;
+		int[] jumsu;
+		jumsu = new int[5];
+
+		for(int i=0; i < 5; i++) {
+			System.out.println(jumsu[i]);
+		}
+		System.out.println(uc);
+		System.out.println(iot);
+    }
+}
