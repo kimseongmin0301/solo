6e16239b41eb07fae6e827302ec98b1646c5c325
Seong min <pose1358@naver.com>
2021-06-23 18:47:02 +0900

    [1차] 비밀지도

--- Study0622.java
+++ Study0622.java
@@ -4,9 +4,9 @@
 */
 public class Study0622 {
     public static void main(String[] args) {
-        int n = 5;
-        int [] arr1 = {9, 20, 28, 18, 11};
-        int [] arr2 = {30, 1, 21, 17, 28};
+        int n = 6;
+        int [] arr1 = {46, 33, 33 ,22, 31, 50};
+        int [] arr2 = {27 ,56, 19, 14, 14, 10};
         String[] answer= new String[n];
         for(int i = 0; i < n; i++){
             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
@@ -16,6 +16,7 @@ public class Study0622 {
             answer[i] = String.format("%" + n + "s", answer[i]);
             answer[i] = answer[i].replaceAll("1", "#");
             answer[i] = answer[i].replaceAll("0", " ");
+            System.out.println(answer[i]);
         }
 
         // 출력
@@ -24,8 +25,6 @@ public class Study0622 {
             System.out.print('"' + answer[i] + '"');
             if(i < n-1)    
                 System.out.print(",");
-            else
-                continue;
         }
         System.out.print("]");
 
